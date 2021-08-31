c35215595a714811bfc8309b38069424f736fd22
SteppenHasen <dustasura@gmail.com>
2021-08-31 19:52:58 +0300

    Make calculations work

--- tip-calculator-app/script.js
+++ tip-calculator-app/script.js
@@ -1,30 +1,37 @@
-function $(x) {return document.getElementById(x);}
+const lets_count = document.getElementById("n_people")
+var tip_value = 0
 
-const lets_count = $("n_people")
-
-var tip_input = Number($("input_tip").text)
-var tip_value = Number($("tip_button").click(function() {
-    this.style.backgroundColor = 'rgb(84, 201, 185)'
-    this.style.color = 'hsl(183, 100%, 15%)'
-    return $(this).val();
-}));
-
-var bill_value = Number($("bill").value);
-var humans_value = Number($("n_people").text);
-
-function calculations(tip, bill, humans) {
-    var tip_per_person = (bill * tip) / (100 * humans)
-    var total_per_person = ((bill * tip / 100) + bill) / humans 
-    $("tip").innerHTML = tip_per_person
-    $("total").innerHTML = total_per_person
-    $("reset").disabled = false
+function func(value) {
+    tip_value = value
 }
 
-if (typeof(bill_value) != number || typeof(humans_value) != number)
-    alert('Please, input a number!')
-else if (tip_value == undefined && tip_input == NaN)
-    alert('Choose tip or write custom tip')
-else if (tip_value != undefined)
-    lets_count.addEventListener('input', calculations(tip_value, bill_value, humans_value))
-else
-    lets_count.addEventListener('input', calculations(tip_input, bill_value, humans_value))
+function calculate() {
+    var tip_input = Number(document.getElementById("input_tip").value)
+    var bill_value = Number(document.getElementById("bill").value);
+    var humans_value = Number(document.getElementById("n_people").value);
+    
+    var temp = bill_value / (100 * humans_value)
+    
+    if (bill_value == 0 || bill_value == NaN) {
+        document.getElementById("bill_alert").innerHTML = 'Enter bill'
+    }
+    if (humans_value == 0 || humans_value == NaN) {
+        document.getElementById("humans_alert").innerHTML = 'Enter number of people'
+    }
+    if ((tip_input == 0 || tip_input == NaN) && tip_value == 0) {
+        document.getElementById("tip_alert").innerHTML = 'Choose your tip'
+    }
+
+    if (tip_input != 0) {
+        document.getElementById("tip").innerHTML = (temp * tip_input).toFixed(2)
+        document.getElementById("total").innerHTML = ((temp * tip_input) + (bill_value / humans_value)).toFixed(2)
+        document.getElementById("reset").disabled = false
+    }
+    else {
+        document.getElementById("tip").innerHTML = (temp * tip_value).toFixed(2)
+        document.getElementById("total").innerHTML = ((temp * tip_value) + (bill_value / humans_value)).toFixed(2)
+        document.getElementById("reset").disabled = false
+    }
+}
+
+lets_count.addEventListener('keyup', calculate)
