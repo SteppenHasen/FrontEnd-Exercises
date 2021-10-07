ff97257e4023908604550361876b7c6af754bf99
SteppenHasen <dustasura@gmail.com>
2021-10-07 16:52:27 +0300

    Done with buttons

--- time-tracking-dashboard-main/script.js
+++ time-tracking-dashboard-main/script.js
@@ -7,7 +7,7 @@ let background_dict = [
     "background-color: hsl(264, 64%, 52%);background-image: url(./images/icon-social.svg);border-radius: 15px;background-repeat: no-repeat;background-position: top right;",
     "background-color: hsl(43, 84%, 65%);background-image: url(./images/icon-self-care.svg);border-radius: 15px;background-repeat: no-repeat;background-position: top right;"
 ]
-let info_area = "height: 12vw;background: hsl(235, 46%, 20%);border: 1px solid hsl(235, 46%, 20%);border-radius: 15px;position: relative;bottom: -20%;"
+
 let request = new XMLHttpRequest()                                     
 request.open('GET', URL);
 request.responseType = 'json';
@@ -17,7 +17,8 @@ request.onload = function() {
     addElement("daily", timeData)
 }
 
-function addElement(a, objData, bg_style_dict=background_dict, info_style=info_area) {
+
+function addElement(a, objData, bg_style_dict=background_dict) {
     for (let i = 0; i < objData.length; i++) {
         let backgroundBox = document.createElement("div")
         backgroundBox.className = "backgroundbox"
@@ -29,14 +30,22 @@ function addElement(a, objData, bg_style_dict=background_dict, info_style=info_a
         let previous_time = objData[i]["timeframes"][a]["previous"]
         let info_box = document.createElement("div")
         info_box.className = "area_info"
-        info_box.style = info_style
         info_box.innerHTML ="<h2>"+objData[i]["title"]+"</h2>\n<h3 id='today_time_work'>"+current_time+"hrs</h3>\n<p id='previous_time_work'>Last Week - "+previous_time+" hrs</p>"
         backgroundBox.appendChild(info_box)
     }
 }
 
-function setTimePeriod(time, data) {
-    let array_to_remove = document.getElementsByClassName("backgroundbox")
-    array_to_remove.forEach(element => { element.remove() });
-    addElement(time, data)
+function setTimePeriod(time) {
+    let request = new XMLHttpRequest()                                     
+    request.open('GET', URL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        let timeData = request.response
+        let array_to_remove = document.getElementsByClassName("backgroundbox")
+        for (let i = array_to_remove.length - 1; i >= 0 ; i--) {
+            array_to_remove[i].remove()
+        }
+        addElement(time, timeData)
+    }
 }
